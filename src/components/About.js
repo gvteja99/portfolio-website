3756803a9cb906d1ed0409aeee7bc5bf8607cb19
gvteja99 <gvteja99@gmail.com>
2023-07-01 09:48:51 -0700

    Changed the layout and added HTTPS support

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ import kubernetesIcon from "@iconify/icons-logos/kubernetes";
 class About extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
-      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+      var profilepic = "images/profile/" + this.props.sharedBasicInfo.image;
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
